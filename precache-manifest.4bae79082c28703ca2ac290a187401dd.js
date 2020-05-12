756780fab3fe4026f19578ee61c1459f40c23ffe
7-baig <saadbaig7777@gmail.com>
2020-05-12 05:39:18 -0700

    Updates

---
+++ precache-manifest.4bae79082c28703ca2ac290a187401dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d094bc53ac3b8e9bee6ad88d9b690feb",
+    "url": "/Hacker-News/index.html"
+  },
+  {
+    "revision": "fbbc13146229db236f6c",
+    "url": "/Hacker-News/static/css/main.83b43477.chunk.css"
+  },
+  {
+    "revision": "5764c85eb768147b7105",
+    "url": "/Hacker-News/static/js/2.edf6b78e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hacker-News/static/js/2.edf6b78e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbbc13146229db236f6c",
+    "url": "/Hacker-News/static/js/main.92edc011.chunk.js"
+  },
+  {
+    "revision": "a853b5e63cadd524d3a0",
+    "url": "/Hacker-News/static/js/runtime-main.89919cee.js"
+  }
+]);
